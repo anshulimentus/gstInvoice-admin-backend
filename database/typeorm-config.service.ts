8373bed74441597bc408766e9fd414a016773642
anshulimentus <anshul.dhakad@imentus.com>
2025-05-31 15:26:39 +0530

    database port update

--- database/typeorm-config.service.ts
+++ database/typeorm-config.service.ts
@@ -15,7 +15,7 @@ export class TypeOrmConfigService implements TypeOrmOptionsFactory {
     return {
         type: "postgres",
         host: process.env.DATABASE_HOST,
-        port: Number(process.env.DATABASE_PORT),
+        port: +(process.env.DATABASE_PORT || 5432),
         username: process.env.DATABASE_USER,
         password: process.env.DATABASE_PASSWORD,
         database: process.env.DATABASE_NAME,
